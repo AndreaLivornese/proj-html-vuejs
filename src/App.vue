f08fd72d8a31cb970eb0389fe10ab98e29afd96d
andrea-livornese <andrealivornese@gmail.com>
2024-03-11 15:59:23 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script >
+
+import AppHeader from './components/AppHeader.vue';
+
+export default{
+
+  components:{
+    AppHeader
+  },
+
+  Data(){
+    return{
+  
+    }
+
+  }
+}
+
+</script>
+
+<template>
+
+  <AppHeader></AppHeader>
+  
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<script >
+
+export default{
+    name:"AppHeader",
+
+    data(){
+        return{
+          
+        }
+
+    }
+}
+
+</script>
+
+<template>
+
+    <Header>
+
+        <div class="container">
+            a
+        </div>
+
+    </Header>
+  
+</template>
+
+<style lang="scss">
+    @use "../style/variables.scss" as *;
+
+    header{
+        background-color: $primaryColor;
+        color:white;
+    }
+
+
+</style>
