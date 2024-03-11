71430c7e8d8e52e651098dd709de9445a243d1e5
andrea-livornese <andrealivornese@gmail.com>
2024-03-11 16:07:39 +0100

    fine parziale dell'header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,6 +6,11 @@ export default{
     data(){
         return{
           
+            position: "North baukhula, tala , USA",
+            email: "deco@example.com",
+
+            timeWork: "8:30AM-8:30PM",
+
         }
 
     }
@@ -18,7 +23,28 @@ export default{
     <Header>
 
         <div class="container">
-            a
+            
+            <div id="inner-header">
+                <div class="info">
+                    <div id="position">
+                        {{ position }}
+                    </div>
+                    <div id="email">
+                        {{ email }}
+                    </div>
+                </div>
+
+                <div class="info">
+                    <div id="work-time">
+                        {{ timeWork }}
+                    </div>
+                    <div id="header-social">
+                        social
+                    </div>
+
+                </div>
+            </div>
+
         </div>
 
     </Header>
@@ -29,8 +55,23 @@ export default{
     @use "../style/variables.scss" as *;
 
     header{
+        padding:10px 0;
+
         background-color: $primaryColor;
         color:white;
+
+        font-size: 10px;
+    }
+
+    #inner-header{
+        display:flex;
+        justify-content: space-between;
+
+        .info{
+            display: flex;
+            gap:30px;
+        }
+
     }
 
 
